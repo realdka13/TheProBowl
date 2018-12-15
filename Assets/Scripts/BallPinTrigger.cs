5c0b5055174bec5660e157d73dc14c5bd4330bec
realdka13 <donovanmagney@yahoo.com>
2018-12-15 12:34:13 -0600

    Added more to the Lane
    Added a ball and pin catch, as well as the managers. Ball now resets after hitting the end of the lane

---
+++ Assets/Scripts/BallPinTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallPinTrigger : MonoBehaviour
+{
+    public GameObject bowlManager;
+
+    //When Ball Triggers the Trigger, destroy and respawn Ball
+    private void OnTriggerEnter(Collider trigObject)
+    {
+        if (trigObject.tag == "BowlingBall")
+        {
+            bowlManager.GetComponent<BowlManager>().ResetBall();
+        }
+    }
+}
---
+++ Assets/Scripts/BowlManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlManager : MonoBehaviour
+{
+
+    public GameObject bowlingBall;
+
+    public void ResetBall()
+    {
+        //Destory Bowling Ball
+        Destroy(bowlingBall);
+
+        //Respawn Bowling Ball
+        bowlingBall = Instantiate(bowlingBall, new Vector3(0, .25f, 0), Quaternion.identity);
+    }
+}
