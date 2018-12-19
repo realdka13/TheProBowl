395224378aeb644263c3fbfc6db255442876c3b1
realdka13 <donovanmagney@yahoo.com>
2018-12-19 17:33:16 -0600

    Scoreboard 2
    Scoreboard complete. including strikes and spares

--- Assets/Scripts/ScoreBoards.cs
+++ Assets/Scripts/ScoreBoards.cs
@@ -7,21 +7,29 @@ public class ScoreBoards : MonoBehaviour
 {
     public Text[] scoreB1 = new Text[21];
     public Text[] sb1FrameTotals = new Text[10];
+    private int player1Frame = 1;
 
     public Text[] scoreB2 = new Text[21];
     public Text[] sb2FrameTotals = new Text[10];
+    private int player2Frame = 1;
 
     public Text[] scoreB3 = new Text[21];
     public Text[] sb3FrameTotals = new Text[10];
+    private int player3Frame = 1;
 
     public Text[] scoreB4 = new Text[21];
     public Text[] sb4FrameTotals = new Text[10];
+    private int player4Frame = 1;
 
     public Text[] scoreB5 = new Text[21];
     public Text[] sb5FrameTotals = new Text[10];
+    private int player5Frame = 1;
 
     public Text[] scoreB6 = new Text[21];
     public Text[] sb6FrameTotals = new Text[10];
+    private int player6Frame = 1;
+
+    private int lastScore;
 
     // Start is called before the first frame update
     void Start()
@@ -76,23 +84,40 @@ public class ScoreBoards : MonoBehaviour
         }
     }
 
-    public void FillScoreBoard(int player, int roll, int score)
+    public void FillScoreBoard(int player, int roll, int score, int playersTotalScore)
     {
         if (player == 1)
         {
-            if (score == 10 && roll % 2 != 0)
+            if (score == 10 && roll % 2 != 0) //If Strike
             {
                 scoreB1[roll].text = "X";
+                player1Frame++;
             }
 
-            else if (score == 0)
+            else if (score == 0)    //If Zero
             {
                 scoreB1[roll - 1].text = "-";
             }
 
             else
             {
-                scoreB1[roll - 1].text = score.ToString();
+                if (roll % 2 == 1)
+                {
+                    lastScore = score;
+                }
+                scoreB1[roll - 1].text = score.ToString(); //All Else(just fill in the number)
+            }
+            if (roll % 2 == 0)
+            {
+                if ((score + lastScore) == 10)//check for spare
+                {
+                    scoreB1[roll - 1].text = "/";
+                }
+                else
+                {
+                    sb1FrameTotals[player1Frame - 1].text = playersTotalScore.ToString(); // Update frame after every other roll
+                }
+                player1Frame++;
             }
         }
         if (player == 2)
@@ -100,6 +125,7 @@ public class ScoreBoards : MonoBehaviour
             if (score == 10 && roll % 2 != 0)
             {
                 scoreB2[roll].text = "X";
+                player2Frame++;
             }
 
             else if (score == 0)
@@ -109,7 +135,23 @@ public class ScoreBoards : MonoBehaviour
 
             else
             {
+                if (roll % 2 == 1)
+                {
+                    lastScore = score;
+                }
                 scoreB2[roll - 1].text = score.ToString();
+            }
+            if (roll % 2 == 0)
+            {
+                if ((score + lastScore) == 10)//check for spare
+                {
+                    scoreB2[roll - 1].text = "/";
+                }
+                else
+                {
+                    sb2FrameTotals[player2Frame - 1].text = playersTotalScore.ToString(); // Update frame after every other roll
+                }
+                player2Frame++;
             }
         }
         if (player == 3)
@@ -117,6 +159,7 @@ public class ScoreBoards : MonoBehaviour
             if (score == 10 && roll % 2 != 0)
             {
                 scoreB3[roll].text = "X";
+                player3Frame++;
             }
 
             else if (score == 0)
@@ -126,7 +169,23 @@ public class ScoreBoards : MonoBehaviour
 
             else
             {
+                if (roll % 2 == 1)
+                {
+                    lastScore = score;
+                }
                 scoreB3[roll - 1].text = score.ToString();
+            }
+            if (roll % 2 == 0)
+            {
+                if ((score + lastScore) == 10)//check for spare
+                {
+                    scoreB3[roll - 1].text = "/";
+                }
+                else
+                {
+                    sb3FrameTotals[player3Frame - 1].text = playersTotalScore.ToString(); // Update frame after every other roll
+                }
+                player3Frame++;
             }
         }
         if (player == 4)
@@ -134,6 +193,7 @@ public class ScoreBoards : MonoBehaviour
             if (score == 10 && roll % 2 != 0)
             {
                 scoreB4[roll].text = "X";
+                player4Frame++;
             }
 
             else if (score == 0)
@@ -143,7 +203,23 @@ public class ScoreBoards : MonoBehaviour
 
             else
             {
+                if (roll % 2 == 1)
+                {
+                    lastScore = score;
+                }
                 scoreB4[roll - 1].text = score.ToString();
+            }
+            if (roll % 2 == 0)
+            {
+                if ((score + lastScore) == 10)//check for spare
+                {
+                    scoreB4[roll - 1].text = "/";
+                }
+                else
+                {
+                    sb4FrameTotals[player4Frame - 1].text = playersTotalScore.ToString(); // Update frame after every other roll
+                }
+                player4Frame++;
             }
         }
         if (player == 5)
@@ -151,6 +227,7 @@ public class ScoreBoards : MonoBehaviour
             if (score == 10 && roll % 2 != 0)
             {
                 scoreB5[roll].text = "X";
+                player5Frame++;
             }
 
             else if (score == 0)
@@ -160,7 +237,23 @@ public class ScoreBoards : MonoBehaviour
 
             else
             {
+                if (roll % 2 == 1)
+                {
+                    lastScore = score;
+                }
                 scoreB5[roll - 1].text = score.ToString();
+            }
+            if (roll % 2 == 0)
+            {
+                if ((score + lastScore) == 10)//check for spare
+                {
+                    scoreB5[roll - 1].text = "/";
+                }
+                else
+                {
+                    sb5FrameTotals[player5Frame - 1].text = playersTotalScore.ToString(); // Update frame after every other roll
+                }
+                player5Frame++;
             }
         }
         if (player == 6)
@@ -168,6 +261,7 @@ public class ScoreBoards : MonoBehaviour
             if (score == 10 && roll % 2 != 0)
             {
                 scoreB6[roll].text = "X";
+                player6Frame++;
             }
 
             else if (score == 0)
@@ -177,7 +271,23 @@ public class ScoreBoards : MonoBehaviour
 
             else
             {
+                if (roll % 2 == 1)
+                {
+                    lastScore = score;
+                }
                 scoreB6[roll - 1].text = score.ToString();
+            }
+            if (roll % 2 == 0)
+            {
+                if ((score + lastScore) == 10)//check for spare
+                {
+                    scoreB6[roll - 1].text = "/";
+                }
+                else
+                {
+                    sb6FrameTotals[player6Frame - 1].text = playersTotalScore.ToString(); // Update frame after every other roll
+                }
+                player6Frame++;
             }
         }
     }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -14,38 +14,50 @@ public class ScoreManager : MonoBehaviour
     private int[] playerFiveScore = new int[21];
     private int[] playerSixScore = new int[21];
 
+    private int player1TotalScore = 0;
+    private int player2TotalScore = 0;
+    private int player3TotalScore = 0;
+    private int player4TotalScore = 0;
+    private int player5TotalScore = 0;
+    private int player6TotalScore = 0;
+
     public void UpdateScore(int player, int roll, int score)
     {
         if (player == 1)
         {
             playerOneScore[roll - 1] = score;
-            print("Player " + player + " Roll " + roll + " with a score of " + score);
+            player1TotalScore += score;
+            scoreBoard.GetComponent<ScoreBoards>().FillScoreBoard(player, roll, score, player1TotalScore);
         }
         else if (player == 2)
         {
             playerTwoScore[roll - 1] = score;
-            print("Player " + player + " Roll " + roll + " with a score of " + score);
+            player2TotalScore += score;
+            scoreBoard.GetComponent<ScoreBoards>().FillScoreBoard(player, roll, score, player2TotalScore);
         }
         else if (player == 3)
         {
             playerThreeScore[roll - 1] = score;
-            print("Player " + player + " Roll " + roll + " with a score of " + score);
+            player3TotalScore += score;
+            scoreBoard.GetComponent<ScoreBoards>().FillScoreBoard(player, roll, score, player3TotalScore);
         }
         else if (player == 4)
         {
             playerFourScore[roll - 1] = score;
-            print("Player " + player + " Roll " + roll + " with a score of " + score);
+            player4TotalScore += score;
+            scoreBoard.GetComponent<ScoreBoards>().FillScoreBoard(player, roll, score, player4TotalScore);
         }
         else if (player == 5)
         {
             playerFiveScore[roll - 1] = score;
-            print("Player " + player + " Roll " + roll + " with a score of " + score);
+            player5TotalScore += score;
+            scoreBoard.GetComponent<ScoreBoards>().FillScoreBoard(player, roll, score, player5TotalScore);
         }
         else if (player == 6)
         {
             playerSixScore[roll - 1] = score;
-            print("Player " + player + " Roll " + roll + " with a score of " + score);
+            player6TotalScore += score;
+            scoreBoard.GetComponent<ScoreBoards>().FillScoreBoard(player, roll, score, player6TotalScore);
         }
-        scoreBoard.GetComponent<ScoreBoards>().FillScoreBoard(player, roll, score);
     }
 }
